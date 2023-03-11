284cd5dc389e100f299eb694fd6244a3b128dae0
Carmelo <carmelojsg@gmail.com>
2023-03-10 23:25:18 -0800

    m

---
+++ sync_configs.sh
@@ -0,0 +1,3 @@
+scp carmelog@192.168.1.12:/home/carmelog/printer_data/config/printer.cfg ./atom/
+scp biqu@192.168.1.11:/home/biqu/printer_data/config/printer.cfg ./voron0/
+scp biqu@192.168.1.11:/home/biqu/printer_data/config/sensorless.cfg ./voron0/
