c3f68987e605cc3b5199d5c9077d9272d010a107
carmelo <carmelojsg@gmail.com>
2024-03-04 23:18:06 -0800

    v0 updates

--- sync_configs.sh
+++ sync_configs.sh
@@ -1,2 +1,2 @@
-scp biqu@192.168.1.11:/home/biqu/printer_data/config/* ./voron0/
+scp biqu@192.168.1.23:/home/biqu/printer_data/config/* ./voron0/
 scp biqu@192.168.1.27:/home/biqu/printer_data/config/* ./voron2-4/
